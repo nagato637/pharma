2001a3a2c1a193d5547be2649a2508c6dd3cb4e3
nagato637 <nagatoakatsu@outlook.com>
2025-05-02 09:00:39 -0700

    second commit

--- produits/models.py
+++ produits/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    quantity = models.IntegerField()
+    productDate = models.DateField(auto_now_add=True)
+    expiryDate = models.DateField()
+    image = models.ImageField(null=True, blank=True, upload_to='produits/products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-productDate']
+
+    def quantity_available(self):
+        if self.quantity == 0:
+            return 'red'
+        elif self.quantity <= 5:
+            return 'orange'
+        else:
+            return 'green'
+
+    def __str__(self):
+        return self.name
