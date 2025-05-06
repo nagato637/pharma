88cc2880456d550c98d9f67ce9307813fff702ca
nagato637 <nagatoakatsu@outlook.com>
2025-05-06 11:10:33 -0700

    third commit

--- produits/models.py
+++ produits/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -30,3 +29,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
