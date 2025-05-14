1c27173167ddac94316c8a67afc2d20a4db30a75
nagato637 <nagatoakatsu@outlook.com>
2025-05-14 14:35:37 -0400

    barca 4 - 3 real

--- produits/views.py
+++ produits/views.py
@@ -14,19 +14,16 @@ class HomeView(ListView):
 
 class AddProduct(CreateView):
     model = Product
-    template_name = 'addproduct.html'
+    template_name = 'adding.html'
     form_class = AddProductForm
     success_url = reverse_lazy('index')
 
 # class UpdateProduct(UpdateView):
 #     model = Product
-#     template_name = 'updateproduct.html'
-#     # form_class = UpdateProductForm
+#     template_name = 'updating.html'
+#     form_class = UpdateProductForm
 #     success_url = reverse_lazy('index')
 
 #     def form_valid(self, form):
-#         product = form.save(commit=False)
-#         if product.expiryDate < datetime.now().date():
-#             messages.error(self.request, "La date d'expiration ne peut pas être dans le passé.")
-#             return self.form_invalid(form)
+#         messages.success(self.request, "Produit mis à jour avec succès.")
 #         return super().form_valid(form)
